1c078b9fe107f5507c9912d48b39850fbd94d75a
LeoTurlock <leo.turlock@gmail.com>
2024-12-16 16:12:58 +0100

    Added footer and drop shadow to navbar.

--- assets/scripts/onLoad.js
+++ assets/scripts/onLoad.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
     $('nav').load('./assets/pages/nav.html');
+    $('footer').load('./assets/pages/footer.html')
     $('.navList').load('./assets/pages/navItems.html')
 });
